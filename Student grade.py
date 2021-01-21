82b5577b4843a036ae5487f351b5237a3918a9c4
DenisGnilamedov <77381857+DenisGnilamedov@users.noreply.github.com>
2021-01-22 00:10:02 +0400

    Student grade.py without ouf , but with a

--- Student grade.py
+++ Student grade.py
@@ -3,6 +3,7 @@ b = 0
 c = 0
 d = 0
 with open(r"C:\1\dataset_3363_4.txt", "r", encoding='utf-8') as lst:
+
     for line in lst:
         line = line.strip().split(';')
         a += int(line[1])
@@ -11,14 +12,13 @@ with open(r"C:\1\dataset_3363_4.txt", "r", encoding='utf-8') as lst:
         d += 1
         mid = (int(line[1])+int(line[2])+int(line[3])/3)
         print(mid)
-        '''for j in lst:
-            with open(r"C:\1\solution.txt", "w") as ouf:
-                print(mid, file=ouf)'''
         mid_sum1 = a / d
         mid_sum2 = b / d
         mid_sum3 = c / d
-
-'''with open(r"C:\1\solution.txt", "w") as ouf:
+    with open(r"C:\1\solution.txt", "a") as ouf:
+        ouf.write(str(mid))
+        ouf.write('\n')
+''' with open(r"C:\1\solution.txt", "w") as ouf:
             ouf.write(str(mid_sum1))
             ouf.write(str(mid_sum2))
             ouf.write(str(mid_sum3))'''
