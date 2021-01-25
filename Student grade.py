566e3964000cdc6ee8125db0a78c7f475a8f6bad
Denis Gnilamedov <denisgnilamedov0810@gmail.com>
2021-01-25 17:21:26 +0400

    solved

--- Student grade.py
+++ Student grade.py
@@ -1,24 +1,21 @@
-a = 0
-b = 0
-c = 0
-d = 0
+a = b = c = d = 0
 with open(r"C:\1\dataset_3363_4.txt", "r", encoding='utf-8') as lst:
-
     for line in lst:
-        line = line.strip().split(';')
-        a += int(line[1])
-        b += int(line[2])
-        c += int(line[3])
-        d += 1
-        mid = (int(line[1])+int(line[2])+int(line[3])/3)
-        print(mid)
+        with open(r"C:\1\solution.txt", "a") as ouf:
+            line = line.strip().split(';')
+            a += int(line[1])
+            b += int(line[2])
+            c += int(line[3])
+            d += 1
+            mid = ((int(line[1])+int(line[2])+int(line[3])) /3)
+            ouf.write(str(mid))
+            ouf.write('\n')
         mid_sum1 = a / d
         mid_sum2 = b / d
         mid_sum3 = c / d
     with open(r"C:\1\solution.txt", "a") as ouf:
-        ouf.write(str(mid))
-        ouf.write('\n')
-''' with open(r"C:\1\solution.txt", "w") as ouf:
-            ouf.write(str(mid_sum1))
-            ouf.write(str(mid_sum2))
-            ouf.write(str(mid_sum3))'''
+        ouf.write(str(mid_sum1) + ' ')
+        ouf.write(str(mid_sum2) + ' ')
+        ouf.write(str(mid_sum3))
+
+
