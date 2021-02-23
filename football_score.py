30ce4dc5eb86422b93fa21f1d2039acc57c164c5
DenisGnilamedov <77381857+DenisGnilamedov@users.noreply.github.com>
2021-02-23 18:50:14 +0400

    football_score_solved.py

--- football_score.py
+++ football_score.py
@@ -1,11 +1,33 @@
+# https://stepik.org/lesson/3380/step/1?unit=963
 n = int(input())
-games = []
-game = {}
-for i in range(n):
-    games.append(input().split(';'))
-print(games)
-for i in games:
-    for j in i:
-        game[[0]] = [1]
-        game[[2]] = [3]
-print(game)
+games = [input().split(';') for i in range(n)]
+temp = [(x[0], x[2]) for x in games]
+team_list = []
+for teams in temp:
+    for team in teams:
+        if team not in team_list:
+            team_list.append(team)
+teams = {team: [0, 0, 0, 0, 0] for team in team_list}
+for team1, score1, team2, score2 in games:
+    if int(score1) == int(score2):
+        teams[team1][0] += 1
+        teams[team2][0] += 1
+        teams[team1][2] += 1
+        teams[team2][2] += 1
+        teams[team1][4] += 1
+        teams[team2][4] += 1
+    elif int(score1) > int(score2):
+        teams[team1][0] += 1
+        teams[team2][0] += 1
+        teams[team1][1] += 1
+        teams[team2][3] += 1
+        teams[team1][4] += 3
+    else:
+        teams[team1][0] += 1
+        teams[team2][0] += 1
+        teams[team2][1] += 1
+        teams[team1][3] += 1
+        teams[team2][4] += 3
+
+for team, team_stat in teams.items():
+    print((team + ':'), *team_stat, end='\n')
